7e03f7867cd7228eb0e7e5be7dfb4f04bfd3fbe0
todorkrastev <krastevtodordinev@yahoo.com>
2024-07-19 22:03:26 +0200

    Initial commit

---
+++ krastevs-gym-rest/src/main/java/com/todorkrastev/krastevsgymrest/KrastevsGymRestApplication.java
@@ -0,0 +1,13 @@
+package com.todorkrastev.krastevsgymrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KrastevsGymRestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KrastevsGymRestApplication.class, args);
+    }
+
+}
---
+++ krastevs-gym-rest/src/test/java/com/todorkrastev/krastevsgymrest/KrastevsGymRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.todorkrastev.krastevsgymrest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KrastevsGymRestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
