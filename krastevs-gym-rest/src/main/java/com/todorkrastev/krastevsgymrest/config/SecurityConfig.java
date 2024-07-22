30af86b88cf135945cd0bd48e8565ecc6ed49679
todorkrastev <krastevtodordinev@yahoo.com>
2024-07-23 01:34:02 +0200

    Add Spring Security

---
+++ krastevs-gym-rest/src/main/java/com/todorkrastev/krastevsgymrest/config/SecurityConfig.java
@@ -0,0 +1,27 @@
+package com.todorkrastev.krastevsgymrest.config;
+
+import com.todorkrastev.krastevsgymrest.filter.JwtAuthenticationFilter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity, JwtAuthenticationFilter jwtAuthenticationFilter) throws Exception {
+        return httpSecurity
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(
+                        authorize ->
+                                authorize.requestMatchers(HttpMethod.GET, "/api/v1/activities/**").permitAll()
+                                        .anyRequest().authenticated()
+                )
+                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
+                .build();
+    }
+}
---
+++ krastevs-gym-rest/src/test/object.js
@@ -0,0 +1,5 @@
+const testObject = {
+    title: 'Test Title 1',
+    description: 'Test Title 1',
+    imageURL: 'Test Image URL 1',
+};
